0f853576ddafd82c94cd4ba758c8975c7da9b08f
Samarth Gaonkar <143213582+SamarthG88@users.noreply.github.com>
2025-07-31 06:51:55 +0000

    two

---
+++ two.js
@@ -0,0 +1,23 @@
+// console.log(Math.abs(-8));
+
+// console.log(Math.round(5.2));
+
+// console.log(Math.ceil(4.1));
+
+// console.log(Math.floor(3.9));
+
+// console.log(Math.min(45,74,1,4));
+// console.log(Math.max(45,74,1,4));
+
+// const min=10;
+// const max=20;
+
+// console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
+
+
+
