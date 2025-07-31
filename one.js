3d26adcbe5d06f3184876f148e6dc96eb21baa04
Samarth Gaonkar <143213582+SamarthG88@users.noreply.github.com>
2025-07-31 06:31:40 +0000

    first

--- one.js
+++ one.js
@@ -1,3 +1,26 @@
-let s="hello";
+let s="hello.MM.HH";
 console.log(s[1]);
+
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+
+console.log(s);
+console.log(s.trim());
+console.log(s.charAt(4));
+console.log(s.indexOf("M"));
+
+console.log(s.substring(1,3));
+
+console.log(s.split("."));
+
+console.log(s.includes("HHKK"));
+
+console.log(s.length);
+
+console.log(s.replace("MM","SS"));
+
+
+
+
+
 
