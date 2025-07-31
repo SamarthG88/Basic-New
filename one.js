a39bb4f36bbc51cbc1f28c4e9ebfa293537e667c
Samarth Gaonkar <143213582+SamarthG88@users.noreply.github.com>
2025-07-31 06:44:14 +0000

    first

--- one.js
+++ one.js
@@ -1,26 +1,49 @@
-let s="hello.MM.HH";
-console.log(s[1]);
+// let s="hello.MM.HH";
+// console.log(s[1]);
 
-console.log(s.toUpperCase());
-console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase());
 
-console.log(s);
-console.log(s.trim());
-console.log(s.charAt(4));
-console.log(s.indexOf("M"));
+// console.log(s);
+// console.log(s.trim());
+// console.log(s.charAt(4));
+// console.log(s.indexOf("M"));
 
-console.log(s.substring(1,3));
+// console.log(s.substring(1,3));
 
-console.log(s.split("."));
+// console.log(s.split("."));
 
-console.log(s.includes("HHKK"));
+// console.log(s.includes("HHKK"));
 
-console.log(s.length);
+// console.log(s.length);
 
-console.log(s.replace("MM","SS"));
-
-
+// console.log(s.replace("MM","SS"));
 
 
 
+// let s="Hello";
+// let t="Morning";
 
+// let g=s;
+
+// g="Good";
+// console.log(g);
+// console.log(s);
+
+// console.log(`${s} ${t}`);
+
+
+const score=100.562;
+const score1=new Number(500);
+
+console.log(score);
+console.log(score1);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+console.log(score.toPrecision(3));
+
+
+const m=1000000;
+console.log(m.toLocaleString("en-IN"));
