b4b8b5c02b1eca75220c52f2855449f88592a24c
Samarth Gaonkar <143213582+SamarthG88@users.noreply.github.com>
2025-08-03 14:49:33 +0000

    eight

---
+++ eight.js
@@ -0,0 +1,35 @@
+// const ar=[10,2,0,25,4];
+
+// for(let i=0; i<ar.length; i++){
+//     console.log(ar[i]);
+// }
+
+
+
+// const ar1=[1,2,3,4,5]
+
+// for(let i=0; i<ar1.length; i++){
+//     for(let j=0; j<ar1.length; j++){
+//         console.log(`outer loop ${ar1[i]} and inner loop ${ar1[j]}`);
+//     }
+// }
+
+
+
+// for(let i=1; i<=10; i++){
+
+//     if(i==5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+// for(let i=1; i<=10; i++){
+
+//     if(i==5){
+//         continue;
+//     }
+//     console.log(i);
+// }
